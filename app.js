6b675513dbd18f081f4ae62dfdc2f1a639a133bd
Claudia Petty <claudialove32@gmail.com>
2020-05-06 18:22:46 -0400

    updated everything after getting html file to style properly, updated functions in routes

--- app.js
+++ app.js
@@ -1,53 +1,64 @@
+//things to require things to decalre
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
 const app = express();
+const fs = require("fs");
 const PORT = 3000;
 
-// Sets up the Express app to handle data parsing
+// these are required for express and linking data between files
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-//=================================================================
-//what variables do we need
-    //a variable for notes (arr)
-    //write variables in ehre
+app.use(express.static('public'));
 
-    const notes = [];
-// ==================================================================
 
-//what routes do we need?
- // homepage
- // /notes where notes can be seen 
- // /notes/add
+//These are my routes
+//index
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
-    app.get("/", function (req, res){
-        //this path goes to index.html
-      res.sendFile(path.join(__dirname, "public/index.html"))
+//this is the route to the main note-taker page
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+//this is the route for reading the JSON for existing notes
+app.get("/api/notes", function (req, res) {
+    fs.readFile("./db/db.json", "utf8", function (err, data) {
+        if (err) throw err;
+        data = JSON.parse(data);
+        res.json(data);
     });
+});
 
-    app.get("/notes", function (req, res){
-    //this path goes to notes.html where user can view notes
-    res.sendFile(path.join(__dirname, "public/notes.html"))
-    //getNotes(); //how do i call this since it lives in another file
+//this is the route for writing and saving new notes usins add functionality
+app.post("/api/notes", function(req, res){
+    console.log(req.body);
+    const newNote = {title: req.body.title, text: req.body.text};
+    console.log("newNote is " + newNote);
+    fs.readFile("./db/db.json", "utf8", function (err, data) {
+        if (err) throw err;
+        data = JSON.parse(data);
+        data.push(newNote);
+        data = JSON.stringify(data);
+        fs.writeFile("./db/db.json", data, function(err){
+            if(err) throw err;
+        });
     });
+    res.json(newNote);
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+  res.sendFile("delete this file");
+});
 
 
-    app.get("/note/add", function (req, res){
-    //adding files uses text box input, not sure what i need to write here +/- how much this conflicts with function in index.js
-    
-    });
 
-    app.get("/note/delete/:id", function (req, res){
-    //there are no objects on screen for activating delete...
-  
-    });
-
-// Starts the server to begin listening
-app.listen(PORT, function() {
+//this starts the server effectively and send console log to let us know its running
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
-// =============================================================
-    
-  
+});
+
+
 
 
